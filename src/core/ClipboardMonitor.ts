01b1aa2a77e54443da2354f387937a055e15b01f
✨NeserCode <nesercode@gmail.com>
2022-12-13 21:12:18 +0800

    update clipboard

--- src/core/ClipboardMonitor.ts
+++ src/core/ClipboardMonitor.ts
@@ -2,9 +2,47 @@ import { clipboard } from "@/utils"
 
 export class ClipboardMonitor {
 	private CLIPBOARD = clipboard
-	private CLIPBOARD_QUEUE: string[] = []
 
 	public getClipboard() {
-		return this.CLIPBOARD.readText()
+		const containsString = this.CLIPBOARD.availableFormats()
+			.join("/")
+			.split("/")
+
+		if (containsString.includes("text"))
+			return {
+				readContent: this.CLIPBOARD.readText(),
+				usefulFormat: this.CLIPBOARD.availableFormats(),
+			}
+		else if (containsString.includes("image"))
+			return {
+				readContent: this.CLIPBOARD.readImage(),
+				usefulFormat: this.CLIPBOARD.availableFormats(),
+			}
+		else if (containsString.includes("html"))
+			return {
+				readContent: this.CLIPBOARD.readHTML(),
+				usefulFormat: this.CLIPBOARD.availableFormats(),
+			}
+		else if (containsString.includes("rtf"))
+			return {
+				readContent: this.CLIPBOARD.readRTF(),
+				usefulFormat: this.CLIPBOARD.availableFormats(),
+			}
+		else if (containsString.includes("bookmark"))
+			return {
+				readContent: this.CLIPBOARD.readBookmark(),
+				usefulFormat: this.CLIPBOARD.availableFormats(),
+			}
+		else
+			return {
+				readContent: this.CLIPBOARD.readText(),
+				usefulFormat: this.CLIPBOARD.availableFormats(),
+			}
+	}
+
+	private listeningClipboard(callback?: (data: string) => void) {
+		this.CLIPBOARD.availableFormats().forEach((format) => {
+			console.log(format, this.CLIPBOARD.read(format))
+		})
 	}
 }
